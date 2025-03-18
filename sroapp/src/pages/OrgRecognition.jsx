f9ab742ac475e065ec7ce706f6325ae691ceb50c
Lance Sacdalan <63360390+sacdalance@users.noreply.github.com>
2025-03-18 17:04:42 +0800

    change

--- sroapp/src/pages/OrgRecognition.jsx
+++ sroapp/src/pages/OrgRecognition.jsx
@@ -2,4 +2,4 @@ const OrgRecognition = () => {
     return <h1 className = "flex justify-center">Organization Recognition Page</h1>;
 };
 
-export default OrgRecognition;
+export default OrgRecognition
