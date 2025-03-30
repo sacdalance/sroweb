96762ed7f8fc6b559b99809dfb9203608bdd4f9b
Lance Sacdalan <63360390+sacdalance@users.noreply.github.com>
2025-03-30 18:58:41 +0800

    add organization

--- database/initDatabase.sql
+++ database/initDatabase.sql
@@ -6,7 +6,16 @@ CREATE TABLE role {
 CREATE TABLE account {
     account_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE CHECK (email LIKE '%up.edu.ph'),
+    email VARCHAR(255) NOT NULL UNIQUE CHECK (email LIKE '%@up.edu.ph'),
     role_id INT REFERENCES role (role_id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 };
+
+CREATE TABLE organization {
+    organization_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    org_email VARCHAR(255) NOT NULL UNIQUE CHECK (email LIKE '%@gmail.com')
+    description TEXT,
+    adviser_name VARCHAR(255),
+    adviser_contact VARCHAR(255)
+}
