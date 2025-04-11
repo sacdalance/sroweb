31720493a83795689e0a1a3079538891352b7c40
Lance Sacdalan <63360390+sacdalance@users.noreply.github.com>
2025-04-11 22:57:24 +0800

    remove env

--- srobackend/server.js
+++ srobackend/server.js
@@ -6,7 +6,7 @@ import { createClient } from '@supabase/supabase-js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
