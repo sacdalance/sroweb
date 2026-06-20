a623dc36b2db390ddd68c3336d5d44eee11c867e
Lance Gabriel Sacdalan <63360390+sacdalance@users.noreply.github.com>
2026-06-20 10:59:20 +0800

    feat: add Vercel Analytics (Insights)

--- sroapp/src/main.jsx
+++ sroapp/src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import AppRoutes from "./routes";
 import "./styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppRoutes />
+    <Analytics />
   </React.StrictMode>
 );
